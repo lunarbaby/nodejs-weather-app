f09b52553be3574ac3641e969afca13387e3611a
Selina Beasley <selina@Selinas-MacBook-Pro.local>
2019-03-16 16:10:09 -0400

    Completed basic node.js tutorial

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const request = require('request');
 
+let apiKey = "0e25992adb7e2be7ecd21a31e99a090a";
+let city = 'atlanta';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
 request(url, function (err, response, body){
 if (err){
     console.log('error:', error);
 } else {
     console.log('body:', body);
+    let weather = JSON.parse(body);
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
 }
 });
+
+
